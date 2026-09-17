ac354dafb7879bec86f19a00ac71b2c96ac8f5cb
Matheus Garcia <tateusg@protonmail.com>
2026-09-16 23:14:26 -0300

    ed.chared.c

--- ed.chared.c
+++ ed.chared.c
@@ -873,7 +873,7 @@ SetKillRing(int max)
 	    for (i = 0; i < KillRingLen; i++) {
 		if (i < count)	/* copy latest */
 		    new[i] = KillRing[j];
-		else		/* free the others */
+		else		/* xfree the others */
 		    xfree(KillRing[j].buf);
 		j = (j + 1) % KillRingLen;
 	    }
