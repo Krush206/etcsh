a385b45be8e4bfd929bf4d50427c06e80e88152c
Matheus Garcia <tateusg@protonmail.com>
2026-09-16 23:14:26 -0300

    tc.str.c

--- tc.str.c
+++ tc.str.c
@@ -612,7 +612,7 @@ bb_cleanup(void *xbb)
 }
 
 void
-bb_free(void *bb)
+bb_xfree(void *bb)
 {
     bb_cleanup(bb);
     xfree(bb);
@@ -698,7 +698,7 @@ STRBUF##_cleanup(void *xbuf)					\
 }								\
 								\
 void								\
-STRBUF##_free(void *xbuf)					\
+STRBUF##_xfree(void *xbuf)					\
 {								\
     STRBUF##_cleanup(xbuf);					\
     xfree(xbuf);						\
