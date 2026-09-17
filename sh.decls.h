51a2eb4fbc23acbd36b76e3dcfe63e55ae34dedb
Matheus Garcia <tateusg@protonmail.com>
2026-09-16 23:14:26 -0300

    sh.decls.h

--- sh.decls.h
+++ sh.decls.h
@@ -35,10 +35,6 @@
 /*
  * sh.c
  */
-extern	void		 *xmalloc	(size_t);
-extern	void		 *xrealloc	(void *, size_t);
-extern	void		 *xcalloc	(size_t, size_t);
-extern	void		  xfree		(void *);
 extern	Char	 	 *gethdir	(const Char *);
 extern	void		  dosource	(Char **, struct command *);
 extern	void		  exitstat	(void) __attribute__((__noreturn__));
@@ -71,7 +67,7 @@ extern	void		  dochngd	(Char **, struct command *);
 extern	Char		 *dnormalize	(const Char *, int);
 extern	void		  dopushd	(Char **, struct command *);
 extern	void		  dopopd	(Char **, struct command *);
-extern	void		  dfree		(struct directory *);
+extern	void		  dxfree		(struct directory *);
 extern	void		  dsetstack	(void);
 extern	const Char	 *getstakd	(int);
 extern	void		  recdirs	(Char *, int);
@@ -189,7 +185,7 @@ extern const struct biltins *isbfunc	(struct command *);
 extern	void		  prvars	(void);
 extern	int		  srchx		(Char *);
 extern	void		  unalias	(Char **, struct command *);
-extern	void		  wfree		(void);
+extern	void		  wxfree		(void);
 extern	void		  dobuiltins	(Char **, struct command *);
 extern	void		  reexecute	(struct command *);
 extern  int		  getYN		(const char *);
@@ -251,7 +247,7 @@ extern	void		  btoeof	(void);
 extern	void		  copylex	(struct wordent *, struct wordent *);
 extern	Char		 *domod		(Char *, Char);
 extern	void		  initlex	(struct wordent *);
-extern	void		  freelex	(struct wordent *);
+extern	void		  xfreelex	(struct wordent *);
 extern	int		  lex		(struct wordent *);
 extern	void		  lex_cleanup	(void *);
 extern	void		  prlex		(struct wordent *);
@@ -266,7 +262,7 @@ extern	ssize_t		  wide_read	(int, Char *, size_t, int);
  */
 extern	int		  any		(const char *, Char);
 extern	Char		**blkcpy	(Char **, Char **);
-extern	void		  blkfree	(Char **);
+extern	void		  blkxfree	(Char **);
 extern	void		  blk_cleanup	(void *);
 extern	void		  blk_indirect_cleanup(void *);
 extern	int		  blklen	(Char **);
@@ -312,14 +308,14 @@ extern	ssize_t		  xread		(int, void *, size_t);
 extern	int		  xtcsetattr	(int, int, const struct termios *);
 extern	ssize_t		  xwrite	(int, const void *, size_t);
 extern	int		  blkcmp	(Char **, Char **);
-extern	void		  blkcmpfree	(Char **, Char **);
+extern	void		  blkcmpxfree	(Char **, Char **);
 extern	void		  blkcmp_cleanup(void *);
 
 /*
  * sh.parse.c
  */
 extern	void		  alias		(struct wordent *);
-extern	void		  freesyn	(struct command *);
+extern	void		  xfreesyn	(struct command *);
 extern struct command 	 *syntax	(const struct wordent *,
 					 const struct wordent *, int);
 extern	void		  syntax_cleanup(void *);
