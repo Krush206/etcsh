54374476c3554902e2f9b2048040ce408b439087
Matheus Garcia <tateusg@protonmail.com>
2026-09-16 23:14:26 -0300

    tc.decls.h

--- tc.decls.h
+++ tc.decls.h
@@ -39,22 +39,10 @@ struct Strbuf;
 /*
  * tc.alloc.c
  */
-#ifndef SYSMALLOC
-#ifndef WINNT_NATIVE
-#ifndef __linux__
-extern	void		  free		(ptr_t);
-extern	memalign_t	  malloc	(size_t);
-extern	memalign_t	  realloc	(ptr_t, size_t);
-extern	memalign_t	  calloc	(size_t, size_t);
-#endif
-#endif /* !WINNT_NATIVE */
-#else /* SYSMALLOC */
-extern	void		  sfree		(ptr_t);
-extern	memalign_t	  smalloc	(size_t);
-extern	memalign_t	  srealloc	(ptr_t, size_t);
-extern	memalign_t	  scalloc	(size_t, size_t);
-#endif /* SYSMALLOC */
-extern	void		  showall	(Char **, struct command *);
+extern	void		  xfree		(void *);
+extern	void		 *xmalloc	(size_t);
+extern	void		 *xrealloc	(void *, size_t);
+extern	void		 *xcalloc	(size_t, size_t);
 
 /*
  * tc.bind.c
@@ -297,7 +285,7 @@ extern  struct blk_buf   *bb_alloc	(void);
 extern	void		  bb_append	(struct blk_buf *, Char *);
 extern	void		  bb_cleanup	(void *);
 extern	Char		**bb_finish	(struct blk_buf *);
-extern  void 		  bb_free	(void *);
+extern  void 		  bb_xfree	(void *);
 
 extern	struct strbuf	 *strbuf_alloc(void);
 extern	void		  strbuf_terminate(struct strbuf *);
@@ -307,7 +295,7 @@ extern  void		  strbuf_appendn(struct strbuf *, const char *,
 extern  void		  strbuf_append (struct strbuf *, const char *);
 extern  char		 *strbuf_finish (struct strbuf *);
 extern	void		  strbuf_cleanup(void *);
-extern	void		  strbuf_free(void *);
+extern	void		  strbuf_xfree(void *);
 extern	struct Strbuf	 *Strbuf_alloc(void);
 extern	void		  Strbuf_terminate(struct Strbuf *);
 extern  void		  Strbuf_append1(struct Strbuf *, Char);
@@ -316,7 +304,7 @@ extern  void		  Strbuf_appendn(struct Strbuf *, const Char *,
 extern  void		  Strbuf_append (struct Strbuf *, const Char *);
 extern  Char		 *Strbuf_finish (struct Strbuf *);
 extern	void		  Strbuf_cleanup(void *);
-extern	void		  Strbuf_free(void *);
+extern	void		  Strbuf_xfree(void *);
 
 
 /*
